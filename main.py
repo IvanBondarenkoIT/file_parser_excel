be684b46ab4850b32829a03d7dbff1ce6d0b61cc
Ivan <ivan.bondarenko.it@gmail.com>
2023-05-23 23:12:23 +0400

    pdf read xls read

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import tabula
 import re
+import pandas as pd
 
 
-def main():
-    pdf_content = tabula.read_pdf("data_pdf.pdf", pages="all")
+def read_xlsx(filename: str):
+    df = pd.read_excel(filename)
+    data_as_list_of_dicts = df.to_dict('records')
+    # print(data_as_list_of_dicts)
+    for _dict in data_as_list_of_dicts:
+        print(_dict)
+
+
+def read_pdf(filename: str):
+    pdf_content = tabula.read_pdf(filename, pages="all")
     text_content = "".join(pdf_content[0])
-    # print(text_content)
 
     all_regex = r"(?<=\rname)(.+?)" \
                 r"\rdate(.+?)" \
@@ -19,6 +27,12 @@ def main():
 
     for match in all_matches:
         print(match)
+
+
+def main():
+    # read_pdf("data_pdf.pdf")
+    read_xlsx("data.xlsx")
+
 
 
 if __name__ == '__main__':
