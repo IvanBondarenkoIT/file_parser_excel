f9a721f57714e96c047821ea2b62d159ba0e9272
Ivan <ivan.bondarenko.it@gmail.com>
2023-05-24 18:14:44 +0400

    xls read csv write add data manager edit

--- data_manager.py
+++ data_manager.py
@@ -1,18 +1,50 @@
-class DataUnit:
+import re
+
+VALIDATION_PATTERNS = {
+    "First Name": r'[A-Za-z]+',
+    "Last Name": r'[A-Za-z]+',
+    "SSN": r'\d{3}-\d{2}-\d{4}',
+    "Address": r'.+',
+    "Company": r'.+',
+    "Department": r'.+',
+    "Position": r'.+',
+    "Zip": r'\d{5}(-\d{4})?',
+    "tel": r'\d?-?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}',
+}
+
+
+class DataInterpreter:
     def __init__(self):
         self.__result_dict = []
+        self.__current_row = None
+        self.__not_valid_data = []
 
     def add_data(self, data):
-        self.__result_dict.append(data)
+        self.__current_row = data
+        self.__result_dict.append({
+            "name": self.validate("First Name"),
+            "address": self.validate("Address"),
+            "user_fullname": f'{self.validate("First Name")} '
+                             f'{self.validate("Last Name")}',
+            "user_additional_info": f'{self.validate("SSN")}|'
+                                    f'{self.validate("Company")}|'
+                                    f'{self.validate("Department")}|'
+                                    f'{self.validate("Position")}|',
+
+            "zip": self.validate("Zip"),
+            "tel": self.validate("Mobile number"),
+            })
+
+    def validate(self, kay):
+        """Return valid values or empty string"""
+        if re.match(pattern=r'[A-Za-z]+', string=str(self.__current_row.get(kay))):
+            return self.__current_row.get(kay)
+        else:
+            self.__not_valid_data.append(self.__current_row.get(kay))
+            return ""
 
     def get_final_values(self):
         return self.__result_dict
 
-
-""",First Name,Last Name,SSN,Address,Company,Department,Position,Zip,Mobile number
-0,Kraig,Pollich,450-70-0086,"972 Joycelyn Causeway, Roobfurt, NC 28368","Gorczany, Hartmann and Lind",IT,dental hygienist,71531-1363,737.591.9348
-1,Manual,Hermiston,100-79-3942,"2960 Reiko Road, Shanahanhaven, ME 77044",Lubowitz-Reichert,IT,astronomer,21603,999.024.7276
-2,Lala,Greenfelder,018-84-1052,"Apt. 876 612 Tromp Forge, Shennafurt, AL 35408-8332","Walsh, Cole and Hahn",IT,carpenter,84030,(552) 622-0475
-3,Claudette,Hermann,237-36-3480,"Suite 576 27978 Angla Mission, Hudsonside, MD 11455",Koss-Reynolds,IT,firefighter,43495-2392,(786) 048-7302
-4,Olinda,Dooley,193-43-2685,"Apt. 205 28009 Roberts Ford, Arnoldfort, WA 91159","Corkery, Lakin and Klocko",IT,carpenter,51094,760.609.8182
-5,Chere,McDermott,107-56-7325,"Apt. 467 471 Schultz Fields, New Kyle, SC 32107",Okuneva-Moen,IT,electrician,80297,874-426-1148"""
+    def get_not_valid_data(self):
+        return self.__not_valid_data
--- main.py
+++ main.py
@@ -1,83 +1,29 @@
 import pandas as pd
-import openpyxl
-import csv
 import data_manager as dm
 
 
-# Example usage
 CSV_FILE_PATH = 'result_csv_file.csv'
 EXCEL_FILE_PATH = 'data.xlsx'
 SHEET_NAME = 'Basic Worksheet'
-
-
-def write_csv(file_path, data, headers="None"):
-    print(data)
-    with open(file_path, 'a', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-
-        # Write headers if provided
-        if headers:
-            writer.writerow(headers)
-
-        # Write data rows
-        for i in data:
-            writer.writerows(i)
-
-
-def validate_data(data):
-    pattern_first_name = r'[A-Za-z]+'
-    pattern_last_name = r'[A-Za-z]+'
-    pattern_ssn = r'\d{3}-\d{2}-\d{4}'
-    pattern_address = r'.+'
-    pattern_company = r'.+'
-    pattern_department = r'.+'
-    pattern_position = r'.+'
-    pattern_zip = r'\d{5}(-\d{4})?'
-    pattern_mobile_number = r'\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}'
 
 
 def read_xlsx(filename: str):
     df = pd.read_excel(filename)
     data_as_list_of_dicts = df.to_dict('records')
     return data_as_list_of_dicts
-    # print(data_as_list_of_dicts)
-    # for _dict in data_as_list_of_dicts:
-    #     yield _dict
-
-
-def read_excel_line_by_line(file_path, sheet_name):
-    wb = openpyxl.load_workbook(file_path)
-    sheet = wb[sheet_name]
-
-    # Get the maximum number of rows and columns in the sheet
-    max_row = sheet.max_row
-    max_column = sheet.max_column
-
-    # Iterate through each row in the sheet
-    for row_num in range(1, max_row + 1):
-        row_data = []
-
-        # Iterate through each column in the row
-        for col_num in range(1, max_column + 1):
-            cell_value = sheet.cell(row=row_num, column=col_num).value
-            row_data.append(cell_value)
-
-        yield row_data
 
 
 def main():
-    # for row in read_excel_line_by_line(EXCEL_FILE_PATH, SHEET_NAME):
-    #     write_csv(CSV_FILE_PATH, row)
-    # read_xlsx(EXCEL_FILE_PATH)
-    data_interpreter = dm.DataUnit()
-    for i in read_xlsx(EXCEL_FILE_PATH):
-        print(i)
-        data_interpreter.add_data(i)
+    data_interpreter = dm.DataInterpreter()
+    for record in read_xlsx(EXCEL_FILE_PATH):
+        print(record)
+        data_interpreter.add_data(record)
 
     df = pd.DataFrame(data_interpreter.get_final_values())
-    df.to_csv("result_csv_file.csv")
+    df.to_csv(CSV_FILE_PATH, index=False)
 
-
+    df = pd.DataFrame(data_interpreter.get_not_valid_data())
+    df.to_csv("not_valid_data.csv", index=False, header=False)
 
 
 if __name__ == '__main__':
